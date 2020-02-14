fa7e81477fb9a3b19d045438a287478d9c855305
whongquan <acupoffancy@gmail.com>
2020-02-14 20:14:17 +0800

    提交背包框架

--- Assets/UI/Bag/Scripts/BagModel.cs
+++ Assets/UI/Bag/Scripts/BagModel.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace Bag {
-	public class BagModel {
+	public class BagModel : MonoBehaviour {
 
 		#region internal data structure
 
 		public class Card {
-			public int id;
 			public string name;
 			public bool ifOwned;
 			public int count;
+
+			public Card(string name) {
+				this.name = name;
+				ifOwned = false;
+				count = 0;
+			}
 		}
 
 		public enum Tab {
@@ -25,12 +31,7 @@ namespace Bag {
 		/// <summary>
 		/// 保存所有卡牌的字典
 		/// </summary>
-		public Dictionary<Tab, List<Card>> allCardsDictionary = new Dictionary<Tab, List<Card>>();
-
-		/// <summary>
-		/// 保存已拥有卡牌的字典
-		/// </summary>
-		public Dictionary<Tab, List<Card>> ownedCardsDictionary = new Dictionary<Tab, List<Card>>();
+		public Dictionary<Tab, List<Card>> cardsModel = new Dictionary<Tab, List<Card>>();
 
 		/// <summary>
 		/// 选项卡列表
@@ -46,14 +47,12 @@ namespace Bag {
 		/// <summary>
 		/// 单例
 		/// </summary>
-		public static BagModel Instance = new BagModel();
+		public static BagModel Instance;
 
-		/// <summary>
-		/// 私有构造器
-		/// </summary>
-		private BagModel() {
+
+		private void Awake() {
+			Instance = this;
 			Init();
-
 		}
 
 		/// <summary>
@@ -63,34 +62,7 @@ namespace Bag {
 
 			// 为数据分配内存空间
 			for (int i = 0; i < tabs.Count; ++i) {
-				allCardsDictionary.Add(tabs[i], new List<Card>());
-				ownedCardsDictionary.Add(tabs[i], new List<Card>());
-			}
-
-		}
-
-		/// <summary>
-		/// 刷新已拥有卡牌字典
-		/// </summary>
-		public void UpdateOwnedCardsDictionary() {
-			for (int i = 0; i < tabs.Count; ++i) {
-				
-				// 获取选项卡对应的卡牌列表
-				List<Card> allCards;
-				allCardsDictionary.TryGetValue(tabs[i], out allCards);
-
-				List<Card> ownedCards;
-				ownedCardsDictionary.TryGetValue(tabs[i], out ownedCards);
-				ownedCards.Clear();
-				
-				// 遍历卡牌列表，更新已拥有卡牌字典数据
-				for (int j = 0; j < allCards.Count; ++j) {
-					Card card = allCards[j];
-					if (card.ifOwned) {
-						ownedCards.Add(card);
-					}
-				}
-
+				cardsModel.Add(tabs[i], new List<Card>());
 			}
 		}
 
