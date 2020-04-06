a43f04ddb8f83cc80c583acb403afbae451e792c
whongquan <acupoffancy@gmail.com>
2020-04-06 15:30:05 +0800

    写入一些新的UI

---
+++ Assets/GameMechanics/GameProcessor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameProcessor : MonoBehaviour {
+
+	public static GameProcessor Instance;
+
+	private void Awake() {
+		Instance = this;
+	}
+
+	public void RoundStarts() {
+		ShowShop();
+
+	}
+
+	private void ShowShop() {
+
+	}
+
+
+	public void RoundEnds() {
+
+	}
+
+}
---
+++ Assets/UI/Shop/Back.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Shop {
+	public class Back : MonoBehaviour, IPointerClickHandler {
+
+		private GameObject shop;
+
+		private void Awake() {
+			shop = transform.parent.gameObject;
+		}
+
+		public void OnPointerClick(PointerEventData eventData) {
+			shop.SetActive(false);
+		}
+	}
+
+}
+
